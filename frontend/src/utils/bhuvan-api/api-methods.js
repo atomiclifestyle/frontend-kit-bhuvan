0ea1d47e6bea46d4f7cae947cf323ef6dcc8c940
Hari Kishore <151401651+atomiclifestyle@users.noreply.github.com>
2025-07-02 04:37:48 +0000

    nginx added

--- frontend/src/utils/bhuvan-api/api-methods.js
+++ frontend/src/utils/bhuvan-api/api-methods.js
@@ -1,8 +1,8 @@
 export const getRouting = async (lat1, lon1, lat2, lon2) => {
     try {
-      const base = import.meta.env.VITE_BASE_URL;
+      // const base = import.meta.env.VITE_BASE_URL;
       const res = await fetch(
-        `${base}/api/bhuvan/routing?lat1=${lat1}&lon1=${lon1}&lat2=${lat2}&lon2=${lon2}`
+        `/api/bhuvan/routing?lat1=${lat1}&lon1=${lon1}&lat2=${lat2}&lon2=${lon2}`
       );
       if (!res.ok) throw new Error('Failed to fetch routing data');
       const json = await res.json();
@@ -14,9 +14,9 @@ export const getRouting = async (lat1, lon1, lat2, lon2) => {
   
   export const getThematicData = async (lat, lon, year) => {
     try {
-      const base = import.meta.env.VITE_BASE_URL;
+      // const base = import.meta.env.VITE_BASE_URL;
       const res = await fetch(
-        `${base}/api/bhuvan/thematic?lat=${lat}&lon=${lon}&year=${year}`
+        `/api/bhuvan/thematic?lat=${lat}&lon=${lon}&year=${year}`
       );
       if (!res.ok) throw new Error('Failed to fetch thematic data');
       const json = await res.json();
@@ -28,8 +28,8 @@ export const getRouting = async (lat1, lon1, lat2, lon2) => {
   
   export const villageGeocoding = async (category) => {
     try {
-      const base = import.meta.env.VITE_BASE_URL;
-      const res = await fetch(`${base}/api/bhuvan/vg?village=${category}`);
+      // const base = import.meta.env.VITE_BASE_URL;
+      const res = await fetch(`/api/bhuvan/vg?village=${category}`);
       if (!res.ok) throw new Error('Failed to add POI');
       const json = await res.json();
       return json;
@@ -40,8 +40,8 @@ export const getRouting = async (lat1, lon1, lat2, lon2) => {
   
  export const getEllipsoid = async (id) => {
   try {
-    const base = import.meta.env.VITE_BASE_URL;
-    const res = await fetch(`${base}/api/bhuvan/ellipsoid?id=${id}`);
+    // const base = import.meta.env.VITE_BASE_URL;
+    const res = await fetch(`/api/bhuvan/ellipsoid?id=${id}`);
     if (!res.ok) throw new Error('Failed to download ellipsoid file');
 
     const blob = await res.blob(); 
@@ -65,9 +65,9 @@ export const getRouting = async (lat1, lon1, lat2, lon2) => {
   
   export const getFloodRunoff = async (catchmentId, outletLat, outletLon) => {
     try {
-      const base = import.meta.env.VITE_BASE_URL;
+      // const base = import.meta.env.VITE_BASE_URL;
       const res = await fetch(
-        `${base}/api/bhuvan/floodrunoff?catchmentId=${catchmentId}&outletLat=${outletLat}&outletLon=${outletLon}`
+        `/api/bhuvan/floodrunoff?catchmentId=${catchmentId}&outletLat=${outletLat}&outletLon=${outletLon}`
       );
       if (!res.ok) throw new Error('Failed to fetch flood runoff data');
       const json = await res.json();
