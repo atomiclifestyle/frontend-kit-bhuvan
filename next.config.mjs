ae37c15d6d1d9f32f1929f6e5429e853f17c8d6d
Hari <151401651+atomiclifestyle@users.noreply.github.com>
2025-07-28 06:49:11 +0000

    next config changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    allowedDevOrigins: ["https://nk47jn-3000.csb.app"],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
